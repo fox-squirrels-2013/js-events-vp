ee97bb2631ce498ef34a9b17af3c6594e885e0d8
vanessa <vipyne@gmail.com>
2013-11-10 20:02:18 -0800

    starting to figure out the js

--- application.js
+++ application.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  var todoTemplate = $.trim($('#todo_template').html());
+console.log("ha")
 
+  var todoTemplate = $.trim($('#todo_template').html());
+  // var task = $('.todo').html()
+  // var task = $('#todo.text').text()
+  
   function bindEvents() {
+    console.log("started")
+    $('.add').on('click', function(e){
+    console.log(todo_template)
+    // $('.todo_list').append(todo_template)
+    })
+
+    
+    // add an event listener for click on delete, complete
+    // .remove()
+
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
   }
@@ -10,15 +24,22 @@ $(document).ready(function() {
 
   
 
-  function buildTodo(todoName) {
-    // Creates an jQueryDOMElement from the todoTemplate.
-    var $todo = $(todoTemplate);
-    // Modifies it's text to use the passed in todoName.
-    $todo.find('h2').text(todoName);
-    // Returns the jQueryDOMElement to be used elsewhere.
-    return $todo;
-  }
-  
+
+
+
+
+
+
+
+  // function buildTodo(todoName) {
+  //   // Creates an jQueryDOMElement from the todoTemplate.
+  //   // console.log(todoName)
+  //   var $todo = $(todoTemplate);
+  //   // Modifies it's text to use the passed in todoName.
+  //   $todo.find('h2').text(todoName);
+  //   // Returns the jQueryDOMElement to be used elsewhere.
+  //   return $todo;
+  // }
 
   bindEvents();
 });
